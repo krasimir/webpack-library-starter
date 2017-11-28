b984d6b688350e34251c60c1819f6e457c62ab59
Krasimir Tsonev <krasimir@outset.ws>
2017-11-28 05:59:13 +0200

    Running dev script and test when building #18

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const path = require('path');
 const env = require('yargs').argv.env; // use --env with webpack 2
 
-let libraryName = 'Library';
+let libraryName = 'library';
 
 let plugins = [], outputFile;
 
