856ea928da7e23cb31b7201dcc6886815b49b233
alvis <alvisisme@163.com>
2018-03-12 17:21:41 +0800

    update output file and test file with correct name.

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,7 @@
 /* global describe, it, before */
 
 import chai from 'chai';
-import {Cat, Dog} from '../lib/library.js';
+import {Cat, Dog} from '../lib/webpack-library-starter.js';
 
 chai.expect();
 
