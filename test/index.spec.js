8c0a37b427302a3ce29f06013ffedef6fbfa4a51
Sébastien Dez <sebdez.dev@gmail.com>
2018-05-04 23:21:19 +0200

    fix: fix broken unit tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,32 +1,32 @@
 /* global describe, it, before */
 
-import chai from 'chai';
-import {Cat, Dog} from '../lib/webpack-library-starter.js';
+import chai from 'chai'
+import { Cat, Dog } from '../lib/npc-generator.js'
 
-chai.expect();
+chai.expect()
 
-const expect = chai.expect;
+const expect = chai.expect
 
-let lib;
+let lib
 
 describe('Given an instance of my Cat library', () => {
   before(() => {
-    lib = new Cat();
-  });
+    lib = new Cat()
+  })
   describe('when I need the name', () => {
     it('should return the name', () => {
-      expect(lib.name).to.be.equal('Cat');
-    });
-  });
-});
+      expect(lib.name).to.be.equal('Cat')
+    })
+  })
+})
 
 describe('Given an instance of my Dog library', () => {
   before(() => {
-    lib = new Dog();
-  });
+    lib = new Dog()
+  })
   describe('when I need the name', () => {
     it('should return the name', () => {
-      expect(lib.name).to.be.equal('Dog');
-    });
-  });
-});
+      expect(lib.name).to.be.equal('Dog')
+    })
+  })
+})
