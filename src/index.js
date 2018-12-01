7b9ede1e0c0a5771c31baa7e70bc34e6b5236be2
Manish Jangir <manish.jangir@globant.com>
2018-12-01 20:27:59 +0530

    Added support for sass and css file import in the library

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import Cat from './cat.js';
 import Dog from './dog.js';
+import './style.scss';
 export { Dog, Cat };
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 const path = require('path');
 const env = require('yargs').argv.env; // use --env with webpack 2
 const pkg = require('./package.json');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 let libraryName = pkg.name;
 
@@ -40,13 +41,23 @@ const config = {
         test: /(\.jsx|\.js)$/,
         loader: 'eslint-loader',
         exclude: /node_modules/
+      },
+      {
+        test:/\.(s*)css$/, 
+        use: ExtractTextPlugin.extract({ 
+          fallback:'style-loader',
+          use:['css-loader','sass-loader'],
+        })
       }
     ]
   },
   resolve: {
     modules: [path.resolve('./node_modules'), path.resolve('./src')],
     extensions: ['.json', '.js']
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin({filename: `${libraryName}.css`})
+  ]
 };
 
 module.exports = config;
